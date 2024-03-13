3c25a44c3aafb97bae5029457c8cb024b21b63f7
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-03-14 04:58:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function daysOfAYear(year) {
+  // Check if the year is divisible by 4
+  if (year % 4 === 0) {
+    // Check if it is divisible by 100
+    if (year % 100 === 0) {
+      // Check if it is divisible by 400
+      if (year % 400 === 0) {
+        return 366; // Leap year
+      } else {
+        return 365; // Not a leap year
+      }
+    } else {
+      return 366; // Leap year
+    }
+  } else {
+    return 365; // Not a leap year
+  }
+}
